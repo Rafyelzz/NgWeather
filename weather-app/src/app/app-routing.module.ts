b5248b070e5cda1ebe5bc71abcbebc5707579889
Rafael <r.munoz@outlook.com>
2018-11-11 19:55:15 +0100

    Fav & bootstrap
    updated fav and added bootstrap

--- weather-app/src/app/app-routing.module.ts
+++ weather-app/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path:'settings',component:SettingsComponent},
-  { path:'home',component:HomeComponent}
+  { path:'home',component:HomeComponent},
+  { path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
 ];
 
 @NgModule({
